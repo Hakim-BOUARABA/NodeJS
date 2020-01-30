9e9929ede146efa1c49b4e80346f4959e3cd11f7
Malik <malik.h@webdevpro.net>
2020-01-30 09:12:06 +0100

    rappel1

--- jour2/00-rappels.js
+++ jour2/00-rappels.js
@@ -1,2 +1,20 @@
 // Rappels jour1
 
+/*
+Node.js => environnement d'exécution de Javascript 
+nous allons pouvoir créer de fichier .js et les exécuter hors d'un navigateur
+
+1 on doit ouvrir un shell 
+2 se placer dans le dossier qui contient le fichier js
+    cd le chemin du dossier qui contient le fichier js
+    cd = change directory = changer de dossier
+3 node nom-du-fichier.js puis Enter
+    Attention !!!!!!!!!! ne pas mettre d'espace / de majuscule / de caractères accentués
+    dans les noms de fichiers 
+
+-----------
+
+exécution dans le shell ET résultat dans le shell aussi 
+
+
+*/

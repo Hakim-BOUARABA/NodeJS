43a47687d07ec7cdfa9445f821951a1498297ade
Malik <malik.h@webdevpro.net>
2020-01-30 16:55:45 +0100

    cas pratique du soir

--- jour2/08-app.js
+++ jour2/08-app.js
@@ -50,7 +50,7 @@ app.post("/nouveau", function(req, res){
     baseDeDonnee.push(nouvelArticle);
     res.send(baseDeDonnee);
 
-})
+});
 
 
 
@@ -75,3 +75,44 @@ DELETE */
     "id" : 4 ,
     "name": "nouveau"
 } */
+
+
+// serveur et on a besoin d'ajouter des nouvelles informations dans un tableau
+
+// POST => méthode HTTP => je souhaite ajouter un item dans un tableau
+
+//=> { id : 1 , titre : "article 1"} objet en javscript
+// => json   { "id" : 1 , "titre" : "article 1"}
+
+// json => envoyer à notre server 
+/* créer un méthode app.post("/nouveau" , function(req , res){
+    const nouveau = req.body ;
+    // ajouter dans notre tableau 
+    méthode crée .push()
+    const tab = [1, 2, 3];
+    tab.push(4); // [1,2,3,4] // .push() méthode pour les variables de type tableau
+    // permet d'ajouter à la fin un nouvel item
+
+    tab.push("bonjour") ; tab vaut = [1,2,3,4 , "bonjour"]
+    const nouveau = req.body ;
+    const nouveau = { "id" : 1 , "titre" : "article 1"}
+    baseDeDonnee.push(nouveau)
+
+    [
+        {
+            id : 1,name : "tuto"
+        },
+        {
+            id : 2 ,name : "js"
+        },
+        {
+            id : 3 ,name : "angular"
+        },
+        { "id" : 1 , "titre" : "article 1"}
+    ]
+
+    res.send(baseDeDonnee)
+
+})
+
+*/
---
+++ jour2/09-cas-pratique-jour.js
@@ -0,0 +1,57 @@
+// https://www.grafikart.fr/tutoriels/express-798
+
+// créer un nouveau fichier 
+// utiliser le module express pour créer un serveur 
+// ce serveur aura les comportements suivants
+
+
+si on l'appelle avec la méthode GET et l'url http://localhost:4300
+
+retourner un tableau 
+
+[
+	{id : 1 , nom : "informatique"},
+	{id : 2 , nom : "management"},
+	{id : 3 , nom : "mathématique"},
+];
+
+
+si on l'appelle avec la méthode GET et l'url http://localhost:4300/matiere/1
+{id : 1 , nom : "informatique"}
+
+si on l'appelle avec la méthode GET et l'url http://localhost:4300/matiere/2
+{id : 2 , nom : "management"}
+
+si l'appeler avec 
+    la méthode POST et 
+    l'url http://localhost:4300/add + 
+    le json {"id" : 4 , "nom" : "autre matière"}
+
+il va vous répondre 
+
+retourner un tableau 
+
+[
+	{id : 1 , nom : "informatique"},
+	{id : 2 , nom : "management"},
+	{id : 3 , nom : "mathématique"},
+    {"id" : 4 , "nom" : "autre matière"}
+];
+
+
+si l'appeler avec 
+    la méthode POST et 
+    l'url http://localhost:4300/new + 
+    le json {"id" : 5 , "categorie" : ["info1, "info2]}
+
+il va vous répondre 
+
+retourner un tableau 
+
+[
+	{id : 1 , nom : "informatique"},
+	{id : 2 , nom : "management"},
+	{id : 3 , nom : "mathématique"},
+    {"id" : 4 , "nom" : "autre matière"},
+    {"id" : 5 , "categorie" : ["info1, "info2]}
+];

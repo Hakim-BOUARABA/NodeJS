aa3f079f1d94a4528475930404b36ac826e062de
Malik <malik.h@webdevpro.net>
2020-01-30 16:22:39 +0100

    premier post

--- jour2/08-app.js
+++ jour2/08-app.js
@@ -12,14 +12,45 @@ const baseDeDonnee = [
         id : 3 ,name : "angular"
     },
 ];
-app.get("/", function(req, res){
+app.get("/", function(req, res){ // http://localhost:3022
     res.send(baseDeDonnee);
 })
 
-app.get("/tuto/:id", function(req, res){
+app.get("/tuto/:id", function(req, res){ // http://localhost:3022/tuto/1
     const id = req.params.id;
     res.send(baseDeDonnee[id -1]);
-})
+});
+
+// http://localhost:3022
+app.post("/" , function(req, res){
+    // ajouter une nouvelle ligne dans le tableau baseDeDonnee
+    const nouvelArticle = {
+        "id" : 4 ,
+        "name": "nouveau"
+    };
+    baseDeDonnee.push(nouvelArticle);
+    res.send(baseDeDonnee);
+});
+
+
 app.listen(3022);
 // Arrêter l'ancien script Ctrl + C puis O Enter
 // nodemon 08-app.js
+
+
+// postman => https://www.getpostman.com/downloads/
+
+// client HTTP 
+// réaliser l'ensemble des requête
+
+/* 
+GET
+PUT 
+POST 
+DELETE */
+
+
+/* {
+    "id" : 4 ,
+    "name": "nouveau"
+} */

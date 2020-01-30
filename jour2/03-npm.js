001b8d39aed7fc99a06515a7fe774e77e25361db
Malik <malik.h@webdevpro.net>
2020-01-30 11:10:06 +0100

    intro npm

--- jour2/03-npm.js
+++ jour2/03-npm.js
@@ -9,3 +9,30 @@
 // npm -v
 
 // outil de type gestionnaire de packet 
+
+// la communauté va créer des modules => libraries = paquet 
+
+// permet de réaliser des actions => vérifier qu'une date est conforme
+// créer un serveur rapidement
+// extraction dans une base de données 
+
+// https://www.npmjs.com/
+// moteur de recherche
+
+// express => permettre de créer un serveur facilement
+// mongoose => permet de manipuler facilement une base de données NoSQL (MongoDB)
+// lodash => librairie qui permet de réaliser facilement des actions
+//            vérifier une date / créer une chiffre aléatoire
+//           utilitaire     
+// nodemon => outil en ligne de commande - 
+//              outil qui permet de redemmarer le shell lorsque l'on fait une modification
+//             dans nos fichiers javascript
+
+// télécharger ces librairies pour les utiliser sur MON Projet
+
+
+// nous allons utiliser npm
+
+
+// npm init --yes => initialiser le projet 
+

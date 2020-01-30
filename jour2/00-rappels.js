dcc774bf2f6799aa0b0425d3b7d5dec3888b4567
Malik <malik.h@webdevpro.net>
2020-01-30 09:20:26 +0100

    suite rappels

--- jour2/00-rappels.js
+++ jour2/00-rappels.js
@@ -17,4 +17,29 @@ nous allons pouvoir créer de fichier .js et les exécuter hors d'un navigateur
 exécution dans le shell ET résultat dans le shell aussi 
 
 
+-----------
+
+nous allons créer des fichiers javascript => fichier = 1 module
+tout ce que vous allez écrire dedans => local (scope = portée des variables)
+
+si je veux découper mon travail sur plusieurs fichiers
+il faut explicitement le dire
+
+dans le fichier qui contient les fonctions et / ou les données 
+
+module.exports.variable = variable
+
+dans le fichier qui va importer les informations 
+
+const {variable} = require("./fichier")
+
+-----------
+
+Il existe des modules natifs => http
+
+ce module très intéressant car => ce que l'on va faire avec Node
+
+Node permet de créer des back office = Serveur
+
+
 */

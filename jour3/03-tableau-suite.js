f3e7b236ab7c54aa8963beaf0dea0ac8e81e15fc
Malik <malik.h@webdevpro.net>
2020-01-31 11:21:50 +0100

    suite tableaux

--- jour3/03-tableau-suite.js
+++ jour3/03-tableau-suite.js
@@ -33,10 +33,74 @@ const t = [
     { id : 10 }
 ];
 
-/* const el = t.find(function(el){
-    return el.id = 10
-});
+// est que j'ai un élément dans ma variable t qui a l'id 10
 
-console.log(where); */
+ const resultats = t.find(function(item){ // el => {id : 1} , puis { id : 2} ... { id : 10 }
+    return item.id === 10 ;
+});  // resultat => si c'est vrai  { id : 10 } si c'est faux undefined
+
+console.log(resultats); /**/
+
+//  vérification 2 == 2 => true 
+//  vérification 2 == "2" => true  => vérifier la valeur
+//  vérification 2 === "2" => false vérifier la valeur ET le type 
+
+// si l'élément est bien présent ALORS je peux récupérer sa position
+
+const position = t.indexOf(resultats) ;
+
+console.log(position);
+
+t[position] = {
+    id: 10,
+    titre : "nouvel article",
+    contenu : "article 1"
+}
+
+console.log(t);
 
 
+
+// en javscript nous allons manipuler deux grands types de tableau
+
+// tableau simple = 
+// const a = [ 1 , 2 , 3] ;
+
+// ajouter un élément à un tableau simple 
+// a.push("hello")
+// supprimer un élément d'un tableau simple
+// a.splice(2,1); 
+// modifier un élément
+// a[index] = "modification"
+
+// tableau complexe = collection 
+/* const b = [
+    {_id: 1},
+    {_id: 2},
+    {_id: 3}
+] 
+// ajouter un élément à un tableau complexe
+b.push(
+    {
+        _id : 4 
+    }
+);
+// supprimer un élément d'un tableau complexe { _id : 2}
+
+*/
+const b = [
+    {_id: 1},
+    {_id: 2},
+    {_id: 3}
+] ;
+// rechercher dans le tableau b
+
+const r = b.find(function(item){
+    return item._id === 2
+}); // r contient la position dans la RAM de { _id : 2}=> passage par référence 
+
+const indexElement = b.indexOf(r);
+
+b.splice(indexElement , 1);
+
+console.log(b)

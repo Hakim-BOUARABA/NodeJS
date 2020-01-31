1a1efada86e6c73c8aaa2d7f7cc7d00c6e5d7443
Malik <malik.h@webdevpro.net>
2020-01-31 12:17:28 +0100

    correction cas n°3

--- jour3/05-exo.js
+++ jour3/05-exo.js
@@ -1,7 +1,8 @@
 // jour3/05-exo.js
 
 //créer la variable  // colllection
-
+// arrêt du shell Ctrl + C
+// nodemon 05-exo.js
 const bdd = [
     {
         _id : 1 ,
@@ -18,9 +19,60 @@ const bdd = [
 ];
 
 // ajouter deux nouveaux produits dans la collection bdd 
-// modifier le prix du produit qui a l' _id 2
+const nouveauProduit1 = {
+    _id : 3 ,
+    titre : "produit 3",
+    contenu : "description du produit 3",
+    prix : 22
+};
+
+const nouveauProduit2 = {
+    _id : 4 ,
+    titre : "produit 4",
+    contenu : "description du produit 4",
+    prix : 25
+};
+
+bdd.push(nouveauProduit1);
+bdd.push(nouveauProduit2);
+console.log(bdd);
+
+// modifier le prix du produit qui a l' _id 2 // _id => MongoDB 
+
+// rechercher le produit qui a l'_id 2
+const produitRecherche = bdd.find(function(item){
+    return item._id === 2;
+});
+
+// récupérer l'index :
+const indexProduitRecherche = bdd.indexOf(produitRecherche);
+
+// modification 
+bdd[indexProduitRecherche].prix = 34;
+
+console.log(bdd);
+
 // modifier le titre du produit qui a l'_id 1
+
+const produitRecherche1 = bdd.find(function(item){
+    return item._id === 1;
+});
+
+// récupérer l'index :
+const indexProduitRecherche1 = bdd.indexOf(produitRecherche1);
+
+// modification 
+bdd[indexProduitRecherche1].titre = "titre modifié";
+
+console.log(bdd);
+
 // supprimer le dernier produit de la collection 
+const nbElementBdd = bdd.length;
+
+bdd.splice(nbElementBdd - 1 , 1);
+
 // afficher la collection
+
+console.log(bdd);
 
 

52d7e65326494e84625a98926bf4585b74dc2526
Malik <malik.h@webdevpro.net>
2020-01-31 11:46:57 +0100

    consigne

---
+++ jour3/05-exo.js
@@ -0,0 +1,25 @@
+// jour3/05-exo.js
+
+//créer la variable  // colllection
+
+const bdd = [
+    {
+        _id : 1 ,
+        titre : "article 1",
+        contenu : "description",
+        prix : 20
+    },
+    {
+        _id : 2 ,
+        titre : "article 2",
+        contenu : "description 2",
+        prix : 203
+    }
+];
+
+// ajouter deux nouveaux produits dans la collection bdd 
+// modifier le prix du produit qui a l' _id 2
+// modifier le titre du produit qui a l'_id 1
+// supprimer le dernier produit de la collection 
+
+

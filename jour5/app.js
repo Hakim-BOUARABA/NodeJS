47c9314011e29c42330ea18963f00c50c13280bc
Malik <malik.h@webdevpro.net>
2020-02-05 09:17:18 +0100

    start jour5

---
+++ jour5/app.js
@@ -0,0 +1,23 @@
+// créer une API : back end complet
+// pour gérer des profils utilisateurs
+// télécharger les modules qui vont être obligatoires
+// pour faire fonctionner notre back End
+// lancer un shell dans le dossier qui contient app.js
+// package.json => npm init --yes
+
+// télécharger les modules dont on a besoin
+
+// npm i express => server
+// npm i @hapi/joi => sécu
+// npm i mongoose => communiquer avec la base de données
+// npm i cors => permettre de communiquer avec notre back end 
+
+// cequ'il faut par défaut
+
+// npm i lodash 
+// nom i bcrypt => fonction qui permettent de crypter certaines données 
+
+
+
+
+
